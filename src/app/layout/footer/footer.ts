18c16afd1f910ae7ca0cc6a7eb88f564b9bde30d
SumitaLot <sumitalot31@gmail.com>
2026-07-16 09:46:38 +0530

    feat(footer): add responsive footer layout

--- src/app/layout/footer/footer.ts
+++ src/app/layout/footer/footer.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, input } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+import { Site } from '../../core/interfaces/site.interface';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterLink,
+  ],
   templateUrl: './footer.html',
   styleUrl: './footer.scss',
 })
-export class Footer {}
+export class Footer {
+
+  readonly site = input.required<Site>();
+
+  readonly currentYear = new Date().getFullYear();
+
+}
